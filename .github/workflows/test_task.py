f01f4af44b16747fda89622dab8cd97601654cbe
acromtech <agib31@gmail.com>
2024-11-20 16:41:37 +0100

    task & test

---
+++ .github/workflows/test_task.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy.testing as npt
+from task import Task
+
+
+class TestTask(unittest.TestCase):
+    def test_matrix_multiplication(self):
+        size = 10
+        task = Task(size=size)
+        task.work()
+
+        # Vérification des résultats
+        npt.assert_allclose(
+            task.a @ task.x,
+            task.b,
+            rtol=1e-5,
+            atol=1e-8,
+            err_msg="Erreur : la solution calculée n'est pas correcte",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
